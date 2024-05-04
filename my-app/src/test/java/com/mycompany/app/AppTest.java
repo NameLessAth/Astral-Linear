0c298bb6d280c02c836379556a280b94a4e18d94
AldyDPP <possiblepiano@gmail.com>
2024-05-04 17:50:08 +0700

    feat: maven setup

---
+++ my-app/src/test/java/com/mycompany/app/AppTest.java
@@ -0,0 +1,22 @@
+package com.mycompany.app;
+
+import static org.junit.Assert.assertTrue;
+
+import com.mpatric.mp3agic.*;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
