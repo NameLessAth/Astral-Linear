5eb73d996e3719bd7576b6c4db9f073a5e2c8de8
RayNoor0 <rayendraalthaf04@gmail.com>
2024-05-21 07:34:07 +0700

    feat: Inactive Deck with bad Card shuffle

--- astrallinear/src/main/java/com/astrallinear/astrallinear/deck/ActiveDeck.java
+++ astrallinear/src/main/java/com/astrallinear/astrallinear/deck/ActiveDeck.java
@@ -1,12 +1,15 @@
 package com.astrallinear.astrallinear.deck;
 
 import java.util.HashMap;
+import java.util.Queue;
 import com.astrallinear.astrallinear.Kartu.*;
 
 public class ActiveDeck {
     private HashMap <Integer, Kartu> content;
+    private InactiveDeck iDeck;
 
     public ActiveDeck(){
+        iDeck = new InactiveDeck();
         content  = new HashMap<>();
         for(int i = 0; i < 6; i++)
             content.put(i, null);
@@ -22,7 +25,32 @@ public class ActiveDeck {
     public void deleteCard(int i){
         content.replace(i, null);
     }
-    
+
+    public int countEmptySlot(){
+        int count = 0;
+        for(int i = 0; i < 6; i++){
+            if(content.get(i) == null)
+                count++;
+        }
+        return count;
+    }
+
+    public void takeShuffle(){
+        int nEmptySlot = this.countEmptySlot();
+        Queue<Kartu> newCards = iDeck.takeCards(nEmptySlot);
+
+        for(int i = 0; i < 6; i++){
+            if(newCards.isEmpty())
+                break;
+            if(content.get(i) == null){
+                content.replace(i, newCards.remove());
+            }
+        }
+    }
+
+    public int getRemainingInactiveDeck(){
+        return iDeck.getCardLeft();
+    }
 }
 
 class EmptyDeckSlotException extends Exception {
