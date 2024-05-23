4fe7d492a8de42c6ffb3a1b0382d5dee69ea5b5c
RayNoor0 <rayendraalthaf04@gmail.com>
2024-05-23 14:14:42 +0700

    feat: take text config (nit tested, no pathing)

--- astrallinear/src/main/java/com/astrallinear/astrallinear/TextConfig/FromTextConfig.java
+++ astrallinear/src/main/java/com/astrallinear/astrallinear/TextConfig/FromTextConfig.java
@@ -5,10 +5,16 @@ import java.io.FileReader;
 import java.io.IOException;
 
 import com.astrallinear.astrallinear.Kartu.Kartu;
+import com.astrallinear.astrallinear.Kartu.KartuHewan;
+import com.astrallinear.astrallinear.Kartu.KartuItem;
+import com.astrallinear.astrallinear.Kartu.KartuMakhluk;
 import com.astrallinear.astrallinear.Kartu.KartuProduk;
+import com.astrallinear.astrallinear.Kartu.KartuTanaman;
 import com.astrallinear.astrallinear.Ladang.Ladang;
 import com.astrallinear.astrallinear.Pemain.Pemain;
 import com.astrallinear.astrallinear.Toko.Toko;
+
+import javafx.util.Pair;
 
 public class FromTextConfig {
 
@@ -19,12 +25,21 @@ public class FromTextConfig {
     private Ladang ladang1;
     private Ladang ladang2;
 
-    public FromTextConfig(String folderPath){
+    public FromTextConfig(String folderPath) throws Exception{
         setState(folderPath + "/gamestate.txt");
-        setPlayer(folderPath);
+        Pair<Pemain, Ladang> temp = setPlayer(folderPath + "/player1.txt");
+        pemain1 = temp.getKey();
+        ladang1 = temp.getValue();
+        temp = setPlayer(folderPath + "/player2.txt");
+        pemain2 = temp.getKey();
+        ladang2 = temp.getValue();
     }
 
-    private void setState(String statePath){
+    public static Pair<Integer, Integer> stringToCoordinate(String s){
+        return new Pair<Integer,Integer>((s.charAt(0)-'A'), (s.charAt(1)*10 + s.charAt(2) - 1));
+    }
+
+    private void setState(String statePath) throws Exception{
         try (BufferedReader br = new BufferedReader(new FileReader(statePath))) {
             String line;
 
@@ -46,13 +61,91 @@ public class FromTextConfig {
             }
 
         } catch (Exception e) {
-            e.printStackTrace();
+            throw e;
         }
     }
 
-    private void setPlayer(String folderPath){
-        ladang1 = new Ladang();
-        ladang2 = new Ladang();
+    private Pair<Pemain, Ladang> setPlayer(String playerPath) throws Exception{
+        String line;
+        try (BufferedReader br = new BufferedReader(new FileReader(playerPath))) {
+            Integer count = 0;
+            Pemain resPemain = new Pemain();
+            Ladang resLadang = new Ladang();
+
+            line = br.readLine();
+            resPemain.increaseGulden(Integer.parseInt(line));
+
+            line = br.readLine();
+            resPemain.getDeck().setRemainingInactiveDeck(Integer.parseInt(line));
+
+            line = br.readLine();
+            count = Integer.parseInt(line);
+
+            for(int i = 0; i < count; i++){
+                line = br.readLine();
+                Kartu kartu;
+                String[] word = line.split("\\s+");
+                Integer pos = stringToCoordinate(word[0]).getKey();
+
+                try{
+                    kartu = new KartuHewan(word[1]);
+                } catch(Exception e){}
+                try{
+                    kartu = new KartuTanaman(word[1]);
+                } catch(Exception e){}
+                try{
+                    kartu = new KartuItem(word[1]);
+                } catch(Exception e){}
+                try{
+                    kartu = new KartuProduk(word[1]);
+                } catch(Exception e){
+                    throw e;
+                }
+                resPemain.getDeck().addKartu(kartu, pos);
+            }
+
+            line = br.readLine();
+            count = Integer.parseInt(line);
+
+            KartuMakhluk kartuMakhluk;
+            Pair<Integer, Integer> coordinate;
+            for(int i = 0; i < count; i++){
+                line = br.readLine();
+                String[] word = line.split("\\s+");
+                coordinate = stringToCoordinate(word[0]);
+                try{
+                    KartuHewan kartuTemp = new KartuHewan(word[1]);
+                    kartuTemp.setBerat(Integer.parseInt(word[2]));
+                    kartuMakhluk = kartuTemp;
+                }catch(Exception e){}
+                try{
+                    KartuTanaman kartuTemp = new KartuTanaman(word[1]);
+                    kartuTemp.setUmur(Integer.parseInt(word[2]));
+                    kartuMakhluk = kartuTemp;
+                }catch(Exception e){
+                    throw e;
+                }
+                resLadang.spawn_at(kartuMakhluk, coordinate.getValue(), coordinate.getKey());
+                Integer count1 = Integer.parseInt(word[3]);
+                for(int j = 0; j < count1; j++){
+                    if(word[i+4].toLowerCase().equals("accelerate"));
+                        resLadang.kartu_accelerate(coordinate.getValue(), coordinate.getKey());
+                    if(word[i+4].toLowerCase().equals("delay"));
+                        resLadang.kartu_delay(coordinate.getValue(), coordinate.getKey());
+                    if(word[i+4].toLowerCase().equals("destroy"));
+                        resLadang.kartu_destroy(coordinate.getValue(), coordinate.getKey());
+                    if(word[i+4].toLowerCase().equals("protect"));
+                        resLadang.kartu_protect(coordinate.getValue(), coordinate.getKey());
+                    if(word[i+4].toLowerCase().equals("instant_harvest"));
+                        resLadang.kartu_instant_harvest(coordinate.getValue(), coordinate.getKey());
+                    if(word[i+4].toLowerCase().equals("trap"));
+                        resLadang.kartu_trap(coordinate.getValue(), coordinate.getKey());
+                }
+            }
+            return new Pair<Pemain,Ladang>(resPemain, resLadang);
+        } catch (Exception e) {
+            throw e;
+        }
     }
 
     public Ladang getLadangSatu(){
