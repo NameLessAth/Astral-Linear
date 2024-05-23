c307813831e8a0cc8fbffd20a68a2a726a7b4529
NameLess <daffakusumamulia@gmail.com>
2024-05-23 14:25:29 +0700

    add : hashmap in kartu makhluk untuk mencatat kartu aktif

--- astrallinear/src/main/java/com/astrallinear/astrallinear/Kartu/KartuMakhluk.java
+++ astrallinear/src/main/java/com/astrallinear/astrallinear/Kartu/KartuMakhluk.java
@@ -1,11 +1,36 @@
 package com.astrallinear.astrallinear.Kartu;
+import java.util.HashMap;
 
 public abstract class KartuMakhluk extends Kartu {
-    
+    private HashMap<String, Integer> itemAktif;
     private KartuProduk drop;
+
     public KartuMakhluk(String nama, KartuProduk drop) {
         super(nama);
         this.drop = drop;
+        this.itemAktif = new HashMap<>();
+    }
+
+    public HashMap<String, Integer> getItemAktif(){
+        return this.itemAktif;
+    }
+
+    public void addItemAktif(KartuItem efek){
+        if (!itemAktif.containsKey(efek.getNama())){
+            itemAktif.put(efek.getNama(), 1);
+        } else{
+            itemAktif.put(efek.getNama(), itemAktif.get(efek.getNama())+1);
+        }
+    }
+
+    public void removeItemAktif(KartuItem efek){
+        if (itemAktif.containsKey(efek.getNama())){
+            itemAktif.put(efek.getNama(), itemAktif.get(efek.getNama())-1);
+            if (itemAktif.get(efek.getNama()) == 0) {
+                itemAktif.remove(efek.getNama());
+            }
+        }
+        // else di sini harusnya throw exception item aktif tidak ditemukan
     }
 
     public KartuProduk panen() throws BelumSiapPanenException {
