b401f5c3e599a6cab57d23aa6cc057e0cc462ec8
DeltDev <deltenc88@gmail.com>
2024-05-25 15:21:32 +0700

    removed blocker because of bugs

--- astrallinear/src/main/java/com/astrallinear/astrallinear/Player1FieldController.java
+++ astrallinear/src/main/java/com/astrallinear/astrallinear/Player1FieldController.java
@@ -518,14 +518,14 @@ public class Player1FieldController implements Initializable{
 
     void OnPanenClick(MouseEvent event) throws IOException {
         System.out.println("OnPanenClick");
-        //block window game utama
-        FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("View/blocker.fxml"));
-        Scene scene = new Scene(fxmlLoader.load());
-        Stage gameStage = gameManager.getGameStage();
-        gameStage.setTitle("Tubes 2 OOP");
-        gameStage.setScene(scene);
-        gameStage.setResizable(false);
-        gameStage.show();
+//        //block window game utama
+//        FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("View/blocker.fxml"));
+//        Scene scene = new Scene(fxmlLoader.load());
+//        Stage gameStage = gameManager.getGameStage();
+//        gameStage.setTitle("Tubes 2 OOP");
+//        gameStage.setScene(scene);
+//        gameStage.setResizable(false);
+//        gameStage.show();
         ImageView source = (ImageView) event.getSource();
         //cari koordinat source
         Integer sourceRow = GridPane.getRowIndex(source);
