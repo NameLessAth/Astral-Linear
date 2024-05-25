d7abb58ae4793080c9c67d5af2fdf61ac900eb83
AldyDPP <possiblepiano@gmail.com>
2024-05-25 08:15:06 +0700

    fix: initializable

---
+++ astrallinear/src/main/java/com/astrallinear/astrallinear/Initializable.java
@@ -0,0 +1,9 @@
+package com.astrallinear.astrallinear;
+
+import javafx.scene.control.Label;
+
+public interface Initializable {
+
+    public void initialize() throws Exception;   
+    public Label getBearTimer();
+}
